006b98248a8027cbfd8fb353d8a68de0fd22cde9
asana <asanali_zhartanov@mail.ru>
2022-02-23 23:17:30 +0300

    hbdcbkjdb

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -103,3 +103,92 @@ public class Ball implements GameConstants {
         }
     }
 }
+
+public class BallComponent extends JPanel implements GameConstants {
+    List<Ball> listBall =  new CopyOnWriteArrayList<>();
+    boolean startClick;
+    public int score=0;
+    public int totalScore=0;
+
+    public void addBall(Ball b){
+        listBall.add(b);
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D)g;
+        for(Ball ball: listBall){
+            g2d.setColor(ball.getColor());
+            g2d.fill(ball.getShape());
+        }
+    }
+
+    public Dimension getPreferredSize() {
+        return new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    }}
+
+public class BallGame implements GameConstants {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                JFrame ballFrame = new BallGameFrame();
+                ballFrame.setVisible(true);
+            }});
+    }}
+
+class BallGameFrame extends JFrame implements GameConstants{
+    private int level=1; //Первый уровень
+    private int ballQnt;
+    private BallComponent ballComponent;
+    private MousePlayer mousePlayerListener;
+
+    //конструктор
+    public BallGameFrame() {
+        ballQnt=STARTQNTBALLS;
+        setTitle("BallGame");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ballComponent = new BallComponent();
+        ballComponent.setBackground(Color.DARK_GRAY);
+        mousePlayerListener = new MousePlayer();
+        add(ballComponent, BorderLayout.CENTER);
+        final JPanel buttonPanel = new JPanel();
+        final JButton startButton = new JButton("Начать игру.");
+        buttonPanel.add(startButton);
+        final JLabel scoreLabel = new JLabel();
+        buttonPanel.add(scoreLabel);
+        startButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                ballComponent.addMouseListener(mousePlayerListener);
+                ballComponent.addMouseMotionListener(mousePlayerListener);
+                startButton.setVisible(false);
+                ballComponent.setCursor(Cursor.getPredefinedCursor(Cursor.CROSSHAIR_CURSOR));
+                startGame(scoreLabel, ballQnt);
+            }});
+        add(buttonPanel, BorderLayout.SOUTH);
+        pack();
+    }
+    public void startGame(JLabel scoreLabel, int ballQnt){
+        Runnable r = new BallRunnable(ballComponent, scoreLabel, level, ballQnt);
+        Thread t = new Thread(r);
+        t.start();
+    }
+
+    class MousePlayer extends MouseAdapter{
+        public void mouseClicked(MouseEvent e) {
+            Random random = new Random();
+
+            Ball ball = new Ball(e.getX(),
+                    e.getY(),
+                    0,
+                    0,
+                    BASERADIUS,
+                    new Color(random.nextInt(255),random.nextInt(255),random.nextInt(255)),
+                    1,
+                    1);
+            ballComponent.startClick=true;
+            ballComponent.addBall(ball);
+
+            ballComponent.removeMouseListener(mousePlayerListener);
+            ballComponent.removeMouseMotionListener(mousePlayerListener);
+            ballComponent.setCursor(Cursor.getDefaultCursor());
+        }}}
